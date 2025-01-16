fc27e050fd80009f8273c5fbc561e26e88c269ca
Kai Herrmann <k.herrmann.to@gmail.com>
2025-01-15 22:44:07 -0500

    Taxlogic

--- obj/Debug/net8.0/MLC.AssemblyInfo.cs
+++ obj/Debug/net8.0/MLC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e46f48897d931635153aa7b1de877d43d26d68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e645b11675fd09bd9fcf5ab581763cfcfa686f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
