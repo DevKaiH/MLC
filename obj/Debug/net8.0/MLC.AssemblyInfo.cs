041c2004007b5600207fa98ec273a7820a329484
Kai Herrmann <k.herrmann.to@gmail.com>
2025-01-18 22:36:28 -0500

    Master/Detail added

--- obj/Debug/net8.0/MLC.AssemblyInfo.cs
+++ obj/Debug/net8.0/MLC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2611eee0621d7ef8b302999f44be36bd1ada09b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5942a6e16482b48a2f6e257fc6c05fb628c613")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
